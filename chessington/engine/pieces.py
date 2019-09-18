99175cd7315766d827f8996da081377b8ca4194c
MOODY Greg <gmoody@altranuk.local>
2019-09-18 10:17:13 +0100

    Pawns can now move

--- chessington/engine/pieces.py
+++ chessington/engine/pieces.py
@@ -35,7 +35,13 @@ class Pawn(Piece):
     """
 
     def get_available_moves(self, board):
-        return []
+        if self.player == Player.WHITE:
+            direction = 1
+        else:
+            direction = -1
+        current_square = board.find_piece(self)
+        next_square = Square.at(current_square.row + direction, current_square.col)
+        return [next_square]
 
 
 class Knight(Piece):
